b763fe98f6c7c96e613c006f441623077f46e14b
Zlati Pehlivanov <zlati.pehlivanov@gmail.com>
2021-10-14 17:10:12 +0300

    wip

--- flutter_id3_reader/lib/flutter_id3_reader.dart
+++ flutter_id3_reader/lib/flutter_id3_reader.dart
@@ -1,19 +1,15 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
 import 'dart:async';
 import 'dart:typed_data';
-import 'package:meta/meta.dart';
+
 import 'package:flutter_id3_reader_platform_interface/flutter_id3_reader_platform_interface.dart';
+
 export 'package:flutter_id3_reader_platform_interface/flutter_id3_reader_platform_interface.dart'
     show SongInfo, TagResponse, AlbumArtRequest;
 
 class FlutterId3Reader {
   static Future<TagResponse> getTag(
     String path, {
-    @required bool remote,
+    bool remote,
   }) async {
     final TagResponse tag =
         await FlutterId3ReaderPlatform.instance.getTag(TagRequest(
@@ -23,13 +19,9 @@ class FlutterId3Reader {
     return tag;
   }
 
-  static Future<Uint8List> getAlbumArt({
-    @required int mediaId
-  }) async {
-    final Uint8List tag =
-        await FlutterId3ReaderPlatform.instance.getAlbumArt(AlbumArtRequest(
-      mediaId: mediaId
-    ));
+  static Future<Uint8List> getAlbumArt({int mediaId}) async {
+    final Uint8List tag = await FlutterId3ReaderPlatform.instance
+        .getAlbumArt(AlbumArtRequest(mediaId: mediaId));
     return tag;
   }
 
